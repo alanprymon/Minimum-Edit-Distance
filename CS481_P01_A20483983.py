f039a7e97f206ff230909569566605f014c40b6d
Alan Prymon <alanprymon@gmail.com>
2024-09-25 15:05:16 -0500

    med and costs started

--- CS481_P01_A20483983.py
+++ CS481_P01_A20483983.py
@@ -1,6 +1,58 @@
 import sys
 import pandas as ps
 
+def del_cost (letter : str) -> int:
+    return 1
+    #if implementing different costs as shown in psuedocode
+    if len(letter) == 1:
+        return -1
+
+def ins_cost(letter: str) -> int:
+    return 1
+    # if implementing different costs as shown in psuedocode
+    if len(letter) == 1:
+        return -1
+
+def sub_cost (incorrect : str, correct : str, isWeighted : bool, weightTable : list) -> int:
+    if not isWeighted:
+        if incorrect == correct:
+            return 0
+        else:
+            return 2
+    else:
+        return -1
+
+#miniumum edit distance
+def med (incorrect : str, correct : str, isWeighted : bool, weightTable : list) -> {int, int}:
+    n = len(incorrect) + 1 #x-axis
+    m = len(correct) + 1 #y-axis
+    disMatrix = [] #will be accessed [x][y]
+    temp = []
+    for i in range(m):
+        temp.append(0)
+    for i in range(n):
+        disMatrix.append(temp.copy())
+    print(disMatrix)
+    for i in range(1, n):
+        disMatrix[i][0] = disMatrix[i - 1][0] + del_cost(incorrect[i - 1])
+    for i in range(1, m):
+        disMatrix[0][i] = disMatrix[0][i - 1] + ins_cost(correct[i - 1])
+    print(disMatrix)
+    if not isWeighted:
+        for i in range(1, n):
+            for j in range(1, m):
+                three = [
+                    disMatrix[i - 1][j] + del_cost(incorrect[i - 1]),
+                    disMatrix[i - 1][j - 1] + sub_cost(incorrect[i - 1], correct[j - 1], isWeighted, weightTable),
+                    disMatrix[i][j - 1] + ins_cost(correct[j - 1])
+                ]
+                three.sort()
+                disMatrix[i][j] = three.pop(0)
+        print(disMatrix)
+        return {disMatrix[n - 1][m - 1], 0}
+    else:
+        return {disMatrix[n - 1][m - 1], -1}
+
 #cleans any empty strings from the list
 def clean_list (clean : list) -> None:
     which = []
@@ -11,7 +63,6 @@ def clean_list (clean : list) -> None:
     for x in which:
         clean.pop(x - count)
         count += 1
-    #print(clean)
     return None
 
 if __name__ == '__main__':
@@ -24,12 +75,10 @@ if __name__ == '__main__':
         weights = True
     else:
         weights = False
-    # print(weights) #boolean
 
     inputword = sys.argv[2].lower()
-    # print(inputword) #string
 
-    #setup words.csv as an list of all words
+    #setup words.csv as a list
     try:
         words_file = open('words.csv', 'rt')
     except FileNotFoundError:
@@ -49,4 +98,9 @@ if __name__ == '__main__':
     clean_list(EDweights)
     for x in range(len(EDweights)):
         EDweights[x] = EDweights[x].split(',')
+
+    med(inputword, 'tests', weights, EDweights)
+
+
+
 
